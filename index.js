ff0a61aab3cbdee7f59f631907a4775dcd5baa0d
Clovizz <clovisluizsjr@gmail.com>
2024-06-02 19:26:40 -0300

    preparando ambiente de desenvolvimento

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const host = '0.0.0.0';
+const porta = 3000;
+const app = express();
+
+app.listen(porta, host, ()=>{
+    console.log(`Servidor rodando em http://${host}:${porta}`);
+})
